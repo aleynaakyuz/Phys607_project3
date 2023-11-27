bf4270a263371936712b554a6cad2a5864a80c38
Aleyna Akyuz <aleyna@Aleyna-MBP-5.lan>
2023-11-26 20:29:39 -0500

    Adding 1D nonconvergent models

--- src/main.py
+++ src/main.py
@@ -1,16 +1,59 @@
 import numpy as np
 import mcmc
+import h5py
 from matplotlib import pyplot as plt
+from pycbc.waveform.waveform import get_fd_waveform
 
-def post(x):
-    return 1 / (2 * np.pi) ** 0.5 * np.exp(-1/2 * (x[0] ** 2.0 + x[1] ** 2.0))
-   
-def proposal(x):
-    return np.random.normal(0, 1, 2) + x
+# Read and add noise to the data
+data = h5py.File('./data/data.h5')
+hp = data['hp'][:]
+dim = hp.shape[0]
+noise = np.random.normal(0, 1, dim)*10**(-24)
+noisy_hp = hp + noise
+
+# Read the parameters
+parameters = {
+    "mass1": "mass1",
+    "mass2": "mass2",
+    "spin1x": "spin1x",
+    "spin1y": "spin1y",
+    "spin1z": "spin1z",
+    "spin2x": "spin2x",
+    "spin2y": "spin2y",
+    "spin2z": "spin2z",
+    "coa_phase": "coa_phase",
+    "inclination": "inclination",
+    "distance": "distance",
+    "ra": "ra",
+    "dec": "dec",
+    "polarization": "polarization"
+}
+
+parameters2 = {
+    "approximant": "IMRPhenomXPHM",
+    "delta_f": 0.1,
+    "f_lower": 10
+}
+
+param_data = h5py.File('Phys607_project3/src/data/parameter.h5')
+data_dic = {key: param_data[dataset_key][:][0] for key, dataset_key in parameters.items()}
+inp = {**data_dic, **parameters2}
+
+def log_likelihood(data, dist, sigma=0.1):
+    inp.update({'distance':dist})
+    predicted_data = get_fd_waveform(**inp) + np.random.normal(0, 1, dim)*10**(-24) 
+    errors = data - predicted_data
+    likelihood = np.log(1 / (2 * np.pi) ** 0.5 * np.exp(-0.5 * np.sum(errors**2) / sigma**2))
+    return likelihood
+
+def prior():
+    pr = np.random.uniform(40000, 50000)  
+    return pr
 
 itt = 1000000
+inital_guess = 49000
 
-chain, prob = mcmc.mcmc(np.array([10, 10]), post, proposal, itt)
+chain, prob = mcmc(inital_guess, noisy_hp, log_likelihood, prior, itt)
 
 plt.figure()
 plt.title("Evolution of the walker")
--- src/mcmc.py
+++ src/mcmc.py
@@ -1,21 +1,20 @@
-# This is a code for 2D mcmc
+# This is a code for 1D mcmc
 
 import numpy as np
 import tqdm
-from matplotlib import pyplot as plt
 
-    
-def mcmc(initial, post, prop, iterations):
+
+def mcmc(initial, data, post, prop, iterations):
     x = [initial]
-    p = [post(x[-1])]
-    for i in tqdm.tqdm(range(iterations)):
+    p = [post(data, x[-1])]
+    for i in tqdm(range(iterations)):
         x_test = prop(x[-1])
-        p_test = post(x_test)
+        p_test = post(data, x_test)
 
         acc = p_test / p[-1]
         u = np.random.uniform(0, 1)
         if u <= acc:
             x.append(x_test)
             p.append(p_test)
-    return np.array(x), np.array(p)
+    return x, p
     
